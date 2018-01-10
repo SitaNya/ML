98afdbe0da589bd18db4ba58b2a716e0b3e7d7c9
张小舟 <zhangxiaozhou@weidian.com>
2018-01-10 15:28:30 +0800

    平行或正交

--- vector/Vector.py
+++ vector/Vector.py
@@ -1,12 +1,17 @@
-from math import sqrt, pi
+from math import sqrt, pi, acos
+from decimal import Decimal, getcontext
+
+getcontext().prec = 30
 
 
 class Vector(object):
+    CANNOT_NORMALIZE_ZERO_VECTOR_MSG = 'Cannot normalize the zero vector'
+
     def __init__(self, coordinates):
         try:
             if not coordinates:
                 raise ValueError
-            self.coordinates = tuple(coordinates)
+            self.coordinates = tuple(Decimal(x) for x in coordinates)
             self.dimension = len(coordinates)
 
         except ValueError:
@@ -24,7 +29,7 @@ class Vector(object):
         return Vector(new_coordinates)
 
     def times_scaler(self, c):
-        new_coordinates = [c * x for x in self.coordinates]
+        new_coordinates = [Decimal(c) * x for x in self.coordinates]
         return Vector(new_coordinates)
 
     def magnitude(self):
@@ -33,22 +38,43 @@ class Vector(object):
 
     def normalized(self):
         try:
-            magnitude = self.magnitude()
-            return self.times_scaler(1. / magnitude)
+            magnitude = Decimal(self.magnitude())
+            return self.times_scaler(Decimal('1.0') / magnitude)
         except ZeroDivisionError:
-            raise Exception("Cannot normalized zero coordinates")
+            raise self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG
 
-    def size(self, v):
-        new_coordinates = [x * y for x, y in zip(self.coordinates, v.coordinates)]
-        return sum(new_coordinates)
+    def dot(self, v):
+        return sum([x * y for x, y in zip(self.coordinates, v.coordinates)])
 
-    def xx(self,v):
-        xxx=self.size(v)/(self.magnitude()*v.magnitude())
-        return xxx
+    def angle_with(self, v, in_degrees=False):
+        try:
+            u1 = self.normalized()
+            u2 = v.normalized()
+            angle_in_radians = acos(Decimal(u1.dot(u2)).quantize(Decimal('0.000')))
 
-    def yy(self,v):
-        xxx = self.size(v) / (self.magnitude() * v.magnitude())
-        return xxx*180/pi
+            if in_degrees:
+                degrees_per_radian = 180. / pi
+                return angle_in_radians * degrees_per_radian
+            else:
+                return angle_in_radians
+        except Exception as e:
+            if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+                raise Exception("Cannot compute an angle with the zero vector")
+            else:
+                raise e
+
+    def is_orthogonal_to(self, v, tolerance=1e-10):
+        return abs(self.dot(v)) < tolerance
+
+    def is_parallel_to(self, v):
+        print self.dot(v)
+        return (self.is_zero() or
+                v.is_zero() or
+                self.angle_with(v) == 0 or
+                self.angle_with(v) == pi)
+
+    def is_zero(self, tolerance=1e-10):
+        return self.magnitude() < tolerance
 
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
@@ -56,22 +82,32 @@ class Vector(object):
     def __eq__(self, v):
         return self.coordinates == v.coordinates
 
-v1=Vector([7.887,4.138])
-w1=Vector([-8.802,6.776])
 
-v2=Vector([3.183,-7.627])
-w2=Vector([-2.668,5.319])
+v1 = Vector([-7.579, -7.88])
+w1 = Vector([22.737, 23.64])
 
-v3=Vector([-5.955,-4.904,-1.874])
-w3=Vector([-4.496,-8.755,7.103])
+v2 = Vector([-2.029, 9.97, 4.172])
+w2 = Vector([-9.231, -6.639, -7.245])
 
-v4=Vector([7.35,0.221,5.188])
-w4=Vector([2.751,8.259,3.985])
+v3 = Vector([-2.328, -7.284, -1.214])
+w3 = Vector([-1.821, 1.072, -2.94])
 
-print v1.size(w1)
+v4 = Vector([2.118, 4.827])
+w4 = Vector([0, 0])
 
-print v2.xx(w2)
 
-print v3.size(w3)
+print v1.is_parallel_to(w1)
 
-print v4.yy(w4)
+print v2.is_parallel_to(w2)
+print v3.is_parallel_to(w3)
+print v4.is_parallel_to(w4)
+
+print "========="
+
+print v1.is_orthogonal_to(w1)
+
+print v2.is_orthogonal_to(w2)
+
+print v3.is_orthogonal_to(w3)
+
+print v4.is_orthogonal_to(w4)
