ee163280e5aed2c44c67ff7af0287786cb56e55b
张小舟 <zhangxiaozhou@weidian.com>
2018-01-27 17:21:48 +0800

    平面

--- vector/line.py
+++ vector/line.py
@@ -136,28 +136,28 @@ class MyDecimal(Decimal):
     def is_near_zero(self, eps=1e-10):
         return abs(self) < eps
 
-
-v1 = Line(normal_vector=Vector([4.046, 2.836]), constant_term=1.21)
-w1 = Line(normal_vector=Vector([10.115, 7.09]), constant_term=3.025)
-
-v2 = Line(normal_vector=Vector([7.204, 3.182]), constant_term=8.68)
-w2 = Line(normal_vector=Vector([8.172, 4.114]), constant_term=9.883)
-
-v3 = Line(normal_vector=Vector([1.182, 5.562]), constant_term=6.774)
-w3 = Line(normal_vector=Vector([1.773, 8.343]), constant_term=9.525)
-
-print v1.is_parallel_to(w1)
-print v1 == w1
-print v1.intersection_with(w1)
-
-print "============="
-
-print v2.is_parallel_to(w2)
-print v2 == w2
-print v2.intersection_with(w2)
-
-print "============="
-
-print v3.is_parallel_to(w3)
-print v3 == w3
-print v3.intersection_with(w3)
+#
+# v1 = Line(normal_vector=Vector([4.046, 2.836]), constant_term=1.21)
+# w1 = Line(normal_vector=Vector([10.115, 7.09]), constant_term=3.025)
+#
+# v2 = Line(normal_vector=Vector([7.204, 3.182]), constant_term=8.68)
+# w2 = Line(normal_vector=Vector([8.172, 4.114]), constant_term=9.883)
+#
+# v3 = Line(normal_vector=Vector([1.182, 5.562]), constant_term=6.774)
+# w3 = Line(normal_vector=Vector([1.773, 8.343]), constant_term=9.525)
+#
+# print v1.is_parallel_to(w1)
+# print v1 == w1
+# print v1.intersection_with(w1)
+#
+# print "============="
+#
+# print v2.is_parallel_to(w2)
+# print v2 == w2
+# print v2.intersection_with(w2)
+#
+# print "============="
+#
+# print v3.is_parallel_to(w3)
+# print v3 == w3
+# print v3.intersection_with(w3)
